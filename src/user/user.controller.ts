956241838268ee6c7d86b2ffd594e838efda178c
vclong2003 <vclong2003@gmail.com>
2024-02-27 17:18:11 +0700

    test: up and running

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller()
-export class UserController {}
+@Controller('user')
+export class UserController {
+  @Get('test')
+  test() {
+    return 'ok';
+  }
+}
